a34d64b0e150141f1af37e30e5f018e39db30456
Evan <evanroot@umich.edu>
2025-07-30 21:06:41 -0500

    add gitignore

---
+++ TodoList_APP/TodoList_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_APPApp.swift
+//  TodoList_APP
+//
+//  Created by Evan Rootness on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
